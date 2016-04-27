3650667555ef335b3089bed349708361000dbbdb
etidalgo <etidalgo@sbs.net.nz>
2016-04-27 16:25:29 +1200

    Add SimpleImplementation

---
+++ SimpleImpl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleLibrary;
+
+namespace Console_SimpleInjector
+{
+    public class SimpleImpl : INodeDescriber
+    {
+        public string Describe(Node node)
+        {
+            return "SimpleImpl";
+        }
+
+    }
+
+    public class BozoImpl : INodeDescriber
+    {
+        public string Describe(Node node)
+        {
+            return "BozoImpl";
+        }
+
+    }
+
+}
